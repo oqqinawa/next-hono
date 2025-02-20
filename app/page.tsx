e336ff65629bec5816c633456a4f278af16f9572
oqqinawa <riyanto.oki@gmail.com>
2025-02-20 12:48:55 +0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+import { useEffect, useState } from "react"
+import "./global.css"
+
+export default function Home() {
+  const [message, setMessage] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/hello")
+      const { message } = await res.json()
+      setMessage(message)
+    }
+    fetchData()
+  }, [])
+
+  if (!message) return <p>Loading...</p>
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p>{message}</p>
+    </div>
+  )
+}
